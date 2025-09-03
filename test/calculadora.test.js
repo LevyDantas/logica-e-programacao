020ee38337233d6b10fe77cda416cfb0a2f39dbc
Levy <levya82@gmail.com>
2025-09-03 16:38:32 -0300

    # desafios-entregues

--- test/calculadora.test.js
+++ test/calculadora.test.js
@@ -2,19 +2,19 @@ const { somarDoisNumeros } = require('../src/calculadora');
 const { expect } = require('chai');
 
 describe('Testes da Função de Soma', function () {
-    it('A função deve ser capaz de somar dois números positivos', function () {
+    it('A função deve ser capaz de somar dois números zerados', function () {
         // Coleta o resultado da função
-        const resultadoDaSoma = somarDoisNumeros(5, 3);
+        const resultadoDaSoma = somarDoisNumeros(0, 0);
 
         //Compara o resultado com o valor que você espera
-        expect(resultadoDaSoma).to.equal(8);
+        expect(resultadoDaSoma).to.equal(0);
     });
 
-    it('A função deve ser capaz de somar um número positivo e um negativo', function() {
+    it('A função deve ser capaz de somar dois números negativos', function() {
         // Coleta o resultado da função
-        const resultadoDaSoma = somarDoisNumeros(50, -15);
+        const resultadoDaSoma = somarDoisNumeros(-50, -15);
 
         //Compara o resultado com o valor que você espera
-        expect(resultadoDaSoma).to.equal(35);
+        expect(resultadoDaSoma).to.equal(-65);
     });
 });
